03b39d81a1ed4a7f0049a9284fc5b63dc1dce6c5
Jordan Sosnowski <jordan.sosnowski@pnnl.gov>
2026-09-15 19:34:27 -0700

    fix(az-login): pass service principal credential via --password flag

--- dotconfig/fish/functions/az-login.fish
+++ dotconfig/fish/functions/az-login.fish
@@ -1,6 +1,7 @@
 function az-login
     _op_load; or return 1
-    AZURE_CLIENT_SECRET="$OP_AZ_CRED" az login --service-principal \
+    az login --service-principal \
         -u "$OP_AZ_USER" \
+        --password "$OP_AZ_CRED" \
         --tenant "$OP_AZ_TENANT"
 end
